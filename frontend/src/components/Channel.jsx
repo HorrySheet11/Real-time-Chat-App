20f1912fa4ceb780d86eedf9afe485757ea83c24
Nat Marn <112994197+HorrySheet11@users.noreply.github.com>
2026-06-17 11:29:22 +0800

    refactor: move older files to new branch and start a new

--- frontend/src/components/Channel.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Channel({id, name, participants }) {
-	return (
-		<div className="channel-item" key={id}>
-			<div>{name}</div>
-			<span>Participants: {participants}</span>
-		</div>
-	);
-}
--- frontend/src/components/Message.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Message({senderName, text}) {
-  return (
-    <div className="message-item">
-      <div><b>{senderName}:</b></div>
-      <span>{text}</span>
-    </div>
-   );
-}
